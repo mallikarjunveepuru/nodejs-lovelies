3c0ebf2d642b77b36a781f999789177e8d110681
Jonathon Orsi <jonathon.orsi@gmail.com>
2015-05-27 01:19:12 -0400

    Revert "Massive layout change. AngularJS integration."
    This reverts commit 195fba5fd9be96fc91db81199c4446b0c9fcdeb5.

--- models/song.js
+++ models/song.js
@@ -5,9 +5,6 @@ var Song = new Schema({
   title: { type: String, required: true },
   artist: { type: String, required: true },
   modified: { type: Date, default: Date.now }
-},
-{
-  versionKey: false 
 });
 
 module.exports = mongoose.model('Song', Song);
